46c3747b131aeef64d53ef1ac4c44421ffa9374f
Maryana <amaryana@gmail.com>
2019-04-14 15:04:19 -0700

    add result display containing value and text

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,48 @@
-$().ready(function(){
-   $("form#price") ,submit(function(event){
-       prevent.eventDefault();
+$(document).ready(function () {
+    $("form#pizza").submit(function (event) {
+        event.preventDefault();
 
-       var userCrust = $("select#crust").val();
-       var userSize = $("select#size").val();
-       var userSauce = $("select#sauce").val();
-       var totalValue = parseInt(userCrust)+parseInt(userSize)+parseInt(userSauce)
+        let userCrust = $("select#crust").val();
+        let userSize = $("select#size").val();
+        let userSauce = $("select#sauce").val();
+        let totalValue = parseInt(userCrust) + parseInt(userSize) + parseInt(userSauce);
+
+        let userCrustText = $("select#crust  option:selected").text();
+        let userSizeText = $("select#size  option:selected").text();
+        let userSauceText = $("select#sauce  option:selected").text();
 
 
-    // back end logic
-    function Pizza (crust,size,sauce)  {
-           this.crust = crust;
-           this.size = size;
+        // back end logic
+        function Pizza(crust, size, sauce) {
+            this.crust = crust;
+            this.size = size;
             this.sauce = sauce;
-    }
-    var getThePrice = function(total){
-           if(total < 6){
-               price = "$5"
-           }else if (total < 8){
-               price = "$10"
-           }else{
-               price = "$20"
-           }
-           return price
-    }
-    var resultPrice = getThePrice(totalValue);
-       var result = "Your pizza is: " + userCrust + userSauce + userSize +". The price is: "+resultPrice;
+        }
 
-       $("#priceOfpizza").text(result);
-       $("hiddenText").show();
-       $("#butTickets").click(function(){
-           $("#firstPage").fadeOut();
-           $("thankYourPage").fadeIn();
-       });
+        let NewUser = new Pizza(userSize, userSauce, userCrust);
+        let total = "";
 
 
-   });
+        let getThePrice = function (total) {
+            if (total < 3) {
+                price = "$5"
+            } else if (total < 5) {
+                price = "$10"
+            } else {
+                price = "$20"
+            }
+            return price;
+        };
+        var resultPrice = getThePrice(totalValue);
+        var result = "Your pizza is: " + userCrustText +' ' + userSizeText+' ' + 'with'+ ' '+ userSauceText+". The price is: " + resultPrice;
+
+        $("#priceOfPizza").text(result);
+        $("#hiddenText").show();
+        $("#buyPizza").click(function () {
+            $("#firstPage").fadeOut();
+            $("thankYourPage").fadeIn();
+        });
+
+
+    });
 });
