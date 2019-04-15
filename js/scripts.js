bacb632ee27f95271f9de3368b7d3f114c56faa5
maryanaantonyuk <amaryana@gmail.com>
2019-04-14 20:08:21 -0700

    add checkboxes (toppings), update JS file with additional prototype,function, display of info

--- js/scripts.js
+++ js/scripts.js
@@ -10,31 +10,51 @@ $(document).ready(function () {
         let userCrustText = $("select#crust  option:selected").text();
         let userSizeText = $("select#size  option:selected").text();
         let userSauceText = $("select#sauce  option:selected").text();
+        function Topping (name,price){
+            this.name = name;
+            this.price = price;
+        }
+        let toppings = [];
+        $("#toppings input:checked").each(function(){
+            let name = $(this).attr("name");
+            let price =parseInt( $(this).val());
+            toppings.push(new Topping(name, price));
+        });
 
-
-        // back end logic
-        function Pizza(crust, size, sauce) {
+        
+        function Pizza(crust, size, sauce, toppings) {
             this.crust = crust;
             this.size = size;
             this.sauce = sauce;
+            this.toppings = toppings;
         }
 
-        let NewUser = new Pizza(userSize, userSauce, userCrust);
+        let pizza = new Pizza(userSize, userSauce, userCrust, toppings);
         let total = "";
 
 
         let getThePrice = function (total) {
-            if (total < 3) {
+            if (total < 5) {
                 price = "$5"
-            } else if (total < 5) {
+            } else if (total < 10) {
                 price = "$10"
             } else {
                 price = "$20"
             }
+
             return price;
         };
+        let allToppingsNames = "";
+        pizza.toppings.forEach(function(topping){
+            totalValue = totalValue + topping.price;
+            allToppingsNames +=" " + topping.name;
+        });
         var resultPrice = getThePrice(totalValue);
-        var result = "Your pizza is: " + userCrustText +' ' + userSizeText+' ' + 'with'+ ' '+ userSauceText+". The price is: " + resultPrice;
+
+
+        var result = "Your pizza is: " + userCrustText + ' ' + userSizeText + ' '
+            + 'with' + ' ' + userSauceText + " and toppings " + allToppingsNames
+            + ". The price is: " + resultPrice;
 
         $("#priceOfPizza").text(result);
         $("#hiddenText").show();
